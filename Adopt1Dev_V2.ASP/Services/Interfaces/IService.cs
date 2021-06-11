cc0f49b5954b19549e1a04f31b135b10cb673cde
isaskou <isabel.skou@yahoo.fr>
2021-06-11 16:08:18 +0200

    add Iservices

---
+++ Adopt1Dev_V2.ASP/Services/Interfaces/IService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Adopt1Dev_V2.ASP.Services.Interfaces
+{
+    public interface IService<T,U>
+        where T: class
+        where U:class
+    {
+        IEnumerable<T> GetAll();
+        U GetById(int id);
+        void Insert(U form);
+        void Update(U form);
+        bool Delete(int id);
+
+    }
+}
