ea8b4bcc04ad3ca3dd525be28dd4b9f859f98d33
isaskou <isabel.skou@yahoo.fr>
2021-06-11 16:20:50 +0200

    add RegisterFormModel and Service

---
+++ Adopt1Dev_V2.ASP/Models/Forms/RegisterForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Adopt1Dev_V2.ASP.Models.Forms
+{
+    public class RegisterForm
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Ce champs est requis")]
+        [MaxLength(50)]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Ce champs est requis")]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Ce champ est requis")]
+        [EmailAddress]
+        [MaxLength(255)]
+        public string Email { get; set; }
+
+        [Required]
+        [MinLength(8, ErrorMessage = "Votre mot de passe doit contenir minimum 8 caractères")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password", ErrorMessage = "Vos mots de passe ne correspondent pas")]
+        [DataType(DataType.Password)]
+        public string RePassword { get; set; }
+
+    }
+}
---
+++ Adopt1Dev_V2.ASP/Services/RegisterService.cs
@@ -0,0 +1,76 @@
+﻿using Adopt1Dev_V2.ASP.Models;
+using Adopt1Dev_V2.ASP.Models.Forms;
+using Adopt1Dev_V2.ASP.Services.Interfaces;
+using Adopt1Dev_V2.DAL;
+using Adopt1Dev_V2.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Adopt1Dev_V2.ASP.Services
+{
+    public class RegisterService : IService<UserModel, RegisterForm>
+    {
+        private readonly DataContext _dc;
+
+        public RegisterService(DataContext dc)
+        {
+            _dc = dc;
+        }
+
+        public UserModel GetByEmailPassword(string email, string password)
+        {
+            //TODO
+            throw new NotImplementedException();
+        }
+
+        public UserModel MapToUserModel(User user)
+        {
+            //TODO
+            throw new NotImplementedException();
+
+        }
+        public void Insert(RegisterForm form)
+        {
+            UserModel model = new UserModel()
+            {
+                Email=form.Email,
+                PasswordIn=form.Password
+            };
+
+            User entity = new User()
+            {
+                Email = model.Email,
+                Password = model.PasswordOut
+            };
+
+            _dc.Users.Add(entity);
+
+            _dc.SaveChanges();
+
+            form.Id = entity.UserId;
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<UserModel> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public RegisterForm GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void Update(RegisterForm form)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
