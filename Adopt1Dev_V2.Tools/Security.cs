4e69bd5d04b504c66baf68f90b9de132ed89f28a
isaskou <isabel.skou@yahoo.fr>
2021-06-13 09:49:05 +0200

    add securityTools

---
+++ Adopt1Dev_V2.Tools/Security.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adopt1Dev_V2.Tools
+{
+    public class Security
+    {
+        public static byte[] HashMe(string passwordIn)
+        {
+            //Je transforme le password en tableau de bytes
+            byte[] data = Encoding.UTF8.GetBytes(passwordIn);
+            byte[] result;
+            //J'instancie la classe qui permet de hasher
+            SHA512 shaM = new SHA512Managed();
+            //Je calcule le sha (Miaouw)
+            result = shaM.ComputeHash(data);
+            return result;
+        }
+    }
+}
